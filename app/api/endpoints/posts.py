66a54ecaa6b1d2d73ad0f8e6630ab8cb4b8b4e01
dean.mcdonald <dean.mcdonald@eigentech.com>
2022-10-24 15:45:33 +0100

    added html endpoint

--- app/api/endpoints/posts.py
+++ app/api/endpoints/posts.py
@@ -1,39 +1,45 @@
 from typing import Any, Dict, List, Optional, Union
 
 from fastapi import APIRouter, Depends
+from fastapi.responses import HTMLResponse
 from pydantic import AnyHttpUrl
-
 from services.facebook import FacebookClient
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
 
 @router.get("/raw/{account_name}")
-def get_facebook_posts(
+def get_raw_post_list(
     account_name: str,
     pages: Union[int, None] = 3,
-    date: Union[int, None] = None,
+    date: Union[str, None] = None,
     client: FacebookClient = Depends(FacebookClient),
 ) -> List[Dict[str, Any]]:
     """
     Fetch all Facebook posts for a given account name
-
-    Args:
-        account_name: The name of the account
-        pages: number of pages to return
-        date: Date to filter posts by in the format "YYYY-DD-MM"
-        client: An instance of `FacebookClient`
-    Returns:
-        A list of Facebook posts for the given `account_name`
     """
     return client.get_raw_posts(account_name, pages=pages, date=date)
 
 
 @router.get("/thumbnails/{account_name}")
-def get_simple_thumbnail_list(
+def get_thumbnail_post_list(
     account_name: str,
     pages: Optional[int] = 3,
     date: Optional[str] = None,
     client: FacebookClient = Depends(FacebookClient),
 ) -> List[Dict[str, Union[str, AnyHttpUrl]]]:
+    """ Fetch a simplified list of posts keyed by thumbnail, text and post_id"""
     return client.get_thumbnail_list(account_name, pages=pages, date=date)
+
+
+@router.get("/html/{account_name}/{post_id}", response_class=HTMLResponse)
+def get_post_as_html(
+    account_name: str,
+    post_id: str,
+    pages: Optional[int] = 3,
+    date: Optional[str] = None,
+    client: FacebookClient = Depends(FacebookClient),
+) -> HTMLResponse:
+    """ Get a simple HTML view of a single post """
+    html_content = client.get_html_post(account_name, post_id, pages=pages, date=date)
+    return HTMLResponse(content=html_content, status_code=200)
--- services/facebook.py
+++ services/facebook.py
@@ -1,9 +1,15 @@
 import datetime
-from pydantic import AnyHttpUrl
+from pydantic import AnyHttpUrl, ValidationError
 
 from typing import Dict, Any, Optional, List, Union
 from datetime import datetime
 from facebook_scraper import get_posts
+from dominate import document
+from dominate.tags import div, img, h5
+
+from app.config import Settings
+
+settings = Settings()
 
 
 class FacebookClient:
@@ -27,6 +33,8 @@ class FacebookClient:
         Returns:
             A list of ordered posts
         """
+        if pages > settings.max_facebook_pages:
+            pages = settings.max_facebook_pages
         posts = list(get_posts(account, pages=pages))
         if date:
             dt = datetime.fromisoformat(date).date()
@@ -36,11 +44,50 @@ class FacebookClient:
     def get_thumbnail_list(
         self,
         account: str,
-        pages: int = 3,
-        date: Optional[str] = None
+        **kwargs
     ) -> List[Dict[str, Union[str, AnyHttpUrl]]]:
-        raw_posts = self.get_raw_posts(account, pages=pages, date=date)
+        """
+        Get a simplified list posts with thumbnail, text and ID
+        Args:
+            account: name of account to to scrape
+        """
+        raw_posts = self.get_raw_posts(account, **kwargs)
         return [
-            {'thumbnail': post['image'], 'text': post['text']}
+            {
+                'thumbnail': post['image'],
+                'text': post['text'],
+                'post_id': post['post_id'],
+            }
             for post in raw_posts if post['image']
         ]
+
+    def get_html_post(
+        self,
+        account: str,
+        post_id: str,
+        **kwargs,
+    ):
+        """
+        Gets a rendered html document content for a given post_id and account_name
+        Args:
+            account: name of accout to scrape
+            post_id: ID of post to display
+        Returns:
+            HTML content for post
+        """
+        thumbnail_posts = self.get_raw_posts(account, **kwargs)
+        selected_post = next(
+            (post for post in thumbnail_posts if post['post_id'] == post_id), None
+        )
+        return self.generate_single_post_html(selected_post) if selected_post else None
+
+    @staticmethod
+    def generate_single_post_html(post: Dict[str, Any]) -> str:
+        """Generates a simple HTML document rendering thumbnail and post text"""
+        # This should really be using templating
+        doc = document(title=post['username'])
+        with doc:
+            with div():
+                img(src=post['image'], style="width:150px;")
+                h5(post['text'])
+        return doc.render()
